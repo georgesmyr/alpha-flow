ef90747320c4c6495e902fdf5fd9f8f29f97b14c
georgesmyr <70004539+georgesmyr@users.noreply.github.com>
2024-01-28 04:17:47 +0100

    Added colored printing function

--- alphaflow/utils.py
+++ alphaflow/utils.py
@@ -7,3 +7,21 @@ def load_config() -> Any:
     with open(config_path, 'r') as file:
         data = json.load(file)
     return data
+
+
+def printc(text: str, color: str) -> None:
+    """ Prints the specified text in the specified color """
+    colors = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        'yellow': '\033[93m',
+        'blue': '\033[94m',
+        'purple': '\033[95m',
+        'cyan': '\033[96m',
+        'white': '\033[97m'
+    }
+    end_color = '\033[0m'
+    if color in colors:
+        print(f"{colors[color]}{text}{end_color}")
+    else:
+        print(text)
